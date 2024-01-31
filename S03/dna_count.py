1fbfd8a9143295390c9eb3d5c2df7ec9c52da3b2
acanoo2023 <a.canoo.2023@alumnos.urjc.es>
2024-01-31 18:02:59 +0100

    S03

---
+++ S03/dna_count.py
@@ -0,0 +1,18 @@
+# Considerations:
+#    You must use a loop for counting the bases.
+#    Suppose that the user always introduce correct sequences (you do not have to manage errors yet).
+
+
+user_seq = input("Enter your sequence: ")
+dna_dict = {}
+
+for i in user_seq:
+    if i not in dna_dict:
+        dna_dict[i] = 1
+    else:
+        dna_dict[i] += 1
+
+print("Total length:", len(user_seq))
+
+for key, value in dna_dict.items():
+    print(key + ":", value)

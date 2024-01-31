f4fc5a43502fdd52e7b8a6feed004a72d6d57b52
acanoo2023 <a.canoo.2023@alumnos.urjc.es>
2024-01-31 18:31:48 +0100

    S03

--- S02/fibonacci.py
+++ S02/fibonacci.py
@@ -1,9 +1,11 @@
-val_1 = 0
-val_2 = 1
+def fibonacci(n):
+    series = [0, 1]
+    for i in range(2, n):
+        next_number = series[-1] + series[-2]
+        series.append(next_number)
+        return series
+n = fibonacci(20)
+print("The 5th Fibonacci number is:", n[5])
+print("The 10th Fibonacci number is:", n[10])
+print("The 15th Fibonacci number is:", n[15])
 
-for i in range(1, 12):
-    if val_1 == 0 and val_2 == 1:
-        print(val_1, end=" ")
-        print(val_2, end=" ")
-
-#NOT FINISHED YET

894451ccaa0f058e1dac5611324d23daed29bde4
acanoo2023 <a.canoo.2023@alumnos.urjc.es>
2024-01-31 18:32:38 +0100

    S03

--- S02/fibonacci.py
+++ S02/fibonacci.py
@@ -4,7 +4,7 @@ def fibonacci(n):
         next_number = series[-1] + series[-2]
         series.append(next_number)
         return series
-n = fibonacci(20)
+n = fibonacci(11)
 print("The 5th Fibonacci number is:", n[5])
 print("The 10th Fibonacci number is:", n[10])
 print("The 15th Fibonacci number is:", n[15])

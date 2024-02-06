035efad78cf9fa4308fe6216747626a7b7008705
acanoo2023 <a.canoo.2023@alumnos.urjc.es>
2024-02-06 16:46:00 +0100

    S04

---
+++ S04/sequence.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+# -- Constant with the name of the file to open
+FILENAME = "sequences/ADA.txt"
+
+# -- Open and read the file
+file_contents = Path(FILENAME).read_text()
+
+list_contents = file_contents.split('\n')
+list_contents.pop(0) # remove header
+
+
+print(len(''.join(list_contents)))
+
+
+
+# ANOTHER WAY:
+
+index = file_contents.find('\n')
+file_contents = (file_contents[index:]).replace('\n', '')
+print(len(file_contents))

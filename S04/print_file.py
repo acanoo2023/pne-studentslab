3109c20f96109b8d27f8f29113363d75ca32c6d0
acanoo2023 <a.canoo.2023@alumnos.urjc.es>
2024-02-06 16:35:15 +0100

    S04

--- S04/print_file.py
+++ S04/print_file.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # -- Constant with the name of the file to open
-FILENAME = "sequences/RNU6_269P.txt"   #To open a file, we must go from the outside to the interior
+FILENAME = "sequences/RNU6_269P.txt"   #(To open a file, we must go from the outside to the interior)
 
 # -- Open and read the file
 file_contents = Path(FILENAME).read_text()

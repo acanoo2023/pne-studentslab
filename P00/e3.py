2c3c6b82126a5f48edfba16f6494e824ae555981
acanoo2023 <a.canoo.2023@alumnos.urjc.es>
2024-02-13 16:25:24 +0100

    Fixing P00

--- P00/e3.py
+++ P00/e3.py
@@ -1,6 +1,8 @@
-from Seq0 import *
+from Seq02 import *
 
 filename_list = ["U5", "ADA", "FRAT1", "FXN"]
 
 for i in filename_list:
-    seq_len(str(i))
+    seq = seq_read_fasta("sequences/" + str(i) + ".txt")
+
+    print("Gene", i, "-> Length:", seq_len(seq))
--- P00/e4.py
+++ P00/e4.py
@@ -1,10 +1,12 @@
-from Seq0 import *
+from Seq02 import *
 
 filename_list = ["U5", "ADA", "FRAT1", "FXN"]
 bases_list = ["A", "C", "T", "G"]
 
 for file in filename_list:
+    seq = seq_read_fasta("sequences/" + file + ".txt")
     print("GENE", file + ":")
     for base in bases_list:
-        seq_count_base(file, base)
+        number = seq_count_base(seq, base)
+        print(" ", base + ":", number)
     print(" ")

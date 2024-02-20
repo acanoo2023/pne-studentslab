97ab9169dc4e36ae8a4b5fa9c9522ce760d404b9
acanoo2023 <a.canoo.2023@alumnos.urjc.es>
2024-02-20 16:45:20 +0100

    P01

--- S06/theory-inheritance.py
+++ S06/theory-inheritance.py
@@ -1,16 +1,27 @@
-class Car:
+class Vehicle:
+    def set_speed(self, speed):
+        self.speed = speed
+
+class Car(Vehicle):
     def __init__(self, brand, speed=0):
         self.car_brand = brand
         self.speed = speed
 
-    def set_speed(self, speed):
-        self.speed = speed
-
 class Ferrari(Car):
-    pass
+    def __init__(self):
+        #Call the init of the mother class
+        super().__init__("Ferrari", 100)
+        self.music = "classic"
+    def make_cabrio(self):
+        self.speed = 20
+        self.music = "loud"
+        return "Wow"
 
 mycar = Car("Renault")
-yourcar = Ferrari("Ferrari")
+yourcar = Ferrari()
 print(yourcar.car_brand)
 yourcar.set_speed(120)
 print(yourcar.speed)
+
+print(yourcar.make_cabrio(), "and music is", yourcar.music, "and speed is", yourcar.speed)
+print(yourcar.make_cabrio(), "and music is", mycar.music, "and speed is", yourcar.speed)

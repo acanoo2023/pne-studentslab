6dd1e0ab21179c0412b9f7319906b13d336960ee
acanoo2023 <a.canoo.2023@alumnos.urjc.es>
2024-02-20 16:46:34 +0100

    P01

--- P01/e5.py
+++ P01/e5.py
@@ -1,23 +1,10 @@
 from Seq1 import Seq
 
-def seq_count(seq):
-    bases_list = ["A", "C", "T", "G"]
-    d = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
-    flag = 0
-    for i in seq:
-        if i == "A" or i == "C" or i == "G" or i == "T":
-            flag += 1
-    if flag == len(seq):
-        for base in bases_list:
-            amount = seq.count(base)
-            d[base] = amount
-    return d
-
+seq_list = [Seq(), Seq("ACTGA"), Seq("Invalid sequence")]
 def print_seqs(seq_list):
     for seq in seq_list:
         position = seq_list.index(seq)
         print(f"Sequence {position}: (Length: {Seq.len(seq)}) {seq}")
-        print(seq_count(seq))
+        print(f"  A: {Seq.count_base(seq, 'A')},  C: {Seq.count_base(seq, 'C')},  G: {Seq.count_base(seq, 'G')},  G: {Seq.count_base(seq, 'T')}")
 
-seq_list = [Seq(), Seq("ACTGA"), Seq("Invalid sequence")]
 print_seqs(seq_list)

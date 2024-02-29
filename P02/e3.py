f082826b9c6f4758e25d848dcd996ad175daf7b5
a.canoo.2023 <a.canoo.2023@alumnos.urjc.es>
2024-02-29 16:56:54 +0100

    .

---
+++ P02/e3.py
@@ -0,0 +1,19 @@
+from Client0 import Client
+
+PRACTICE = 2
+EXERCISE = 3
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Parameters of the server to talk to
+IP = "10.1.131.225" # your IP address
+PORT = 5362
+
+# -- Create a client object
+c = Client(IP, PORT)
+
+# -- Send a message to the server
+print("Sending a message to the server...")
+response = c.talk("Testing!!!")  #Here I put the message that I want to send to the server
+print(f"Response: {response}")
+
--- P02/trying.py
+++
@@ -1,5 +0,0 @@
-print("I am trying from my home")
-
-for i in range(0, 10):
-    print(i, end=",")
-print("10")
